13e7021da283972fb9da3d8741d5aa334aaeffe5
Noah <noahcuroe@gmail.com>
2023-07-17 19:58:19 -0500

    footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { DividerHeading } from "../../components/DividerHeading/DividerHeading";
+import { IconText } from "../../components/IconText/IconText";
 import { ImageCard } from "../../components/ImageCard/ImageCard";
 import { Teaser } from "../../components/Teaser/Teaser";
 import { PageLayout } from "../../containers/PageLayout/PageLayout";
