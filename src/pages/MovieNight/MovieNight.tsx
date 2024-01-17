29504df0172edba1da5cbe03e137ecd62e3f72cf
travisfriesen <github@travisfriesen.ca>
2024-01-16 21:19:21 -0600

    movie night page + fetch event data

--- src/pages/MovieNight/MovieNight.tsx
+++ src/pages/MovieNight/MovieNight.tsx
@@ -1,9 +1,11 @@
-import { Text } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { Text, AspectRatio, Image } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { PageLayout } from "../../containers/PageLayout/PageLayout";
-import { MovieCard } from "../../components/MovieCard/MovieCard";
+import { EventCard } from "../../components/EventCard/EventCard";
+import { error } from "console";
 
-interface IEventData {
+interface IMovieData {
+    movie: string;
     title: string;
     image: string;
     start: string;
@@ -17,14 +19,45 @@ interface IEventData {
 }
 
 export const MovieNight = () => {
-      
-      
+    const [movies, setMovies] = useState<IMovieData[]>([]);
+
+    useEffect(() => {
+        fetch('/data/events.json')
+            .then(response => response.json())
+            .then((data: { movie: IMovieData }[]) => {
+                const movieData = data.map(item => item.movie);
+                setMovies(movieData);
+            })
+            .catch(error => console.error('Error:', error));
+    }, []);
+        
+
     return (
         <PageLayout title="Movie Nights" image="/img/backgrounds/home.jpg">
-            <Text>
-                The CSSA is trying to host weekly movie nights! 
-            
-            </Text>
+            {movies.map((movie, index) => {
+                const startTime = new Date(movie.start).toLocaleString('en-US', { dateStyle: 'long', timeStyle: 'short' });
+                const endTime = new Date(movie.end).toLocaleString('en-US', { dateStyle: 'long', timeStyle: 'short' });
+
+                return (
+                    <div key={index}>
+                        <Text fontSize={'md'}fontWeight={'800'}lineHeight={'10'}>
+                        {movie.status} <br />
+                    </Text>
+                    <Text>
+                        The CSSA is trying to host weekly nights! <br />
+                        <br />
+                        Our next movie night we are watching: {movie.moviename}<br />
+                        The movie will be on {startTime} and be hosted in the {movie.location} <br />
+                    </Text>
+                    <br />
+                    <AspectRatio ratio={8.5 / 11}>
+                    <Image src={movie.poster} />
+                </AspectRatio>
+                    
+                </div>
+                );
+            })}
         </PageLayout>
     );
 }
+
