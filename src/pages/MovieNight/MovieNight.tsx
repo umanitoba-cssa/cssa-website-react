39f075ba02bbeb07a1917c700dea90819f0e54ea
Travis <github@travisfriesen.ca>
2024-03-11 22:45:35 -0500

    movie night update into main (#54) (#55)
    * movie night update
    
    * removed old assets

--- src/pages/MovieNight/MovieNight.tsx
+++ src/pages/MovieNight/MovieNight.tsx
@@ -48,7 +48,7 @@ export const MovieNight = () => {
                         The movie will be on {startTime} and be hosted in the {movie.location} <br />
                     </Text>
                     <br />
-                    <AspectRatio ratio={1 / 1}>
+                    <AspectRatio ratio={9 / 12.73}>
                     <Image src={movie.poster} />
                 </AspectRatio>
                     
